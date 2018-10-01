6b41464311f18eab1503ef3894cd202229f64e14
victorbatista07 <joao.victor.zaga@gmail.com>
2018-10-01 01:39:35 +0000

    Add dependences

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const restify = require('restify');
 const plugins = require('restify-plugins');
 
-require('dotenv').config()
-
 const server = restify.createServer();
 
 server.use(plugins.acceptParser(server.acceptable));
 server.use(plugins.bodyParser());
 
-server.post('/soma', (req, resp, next) => {
-	res.send(200, req.body.reduce((a,b) => a + b, 0));
+server.post('/sum', (req, res, next) => {
+	res.send(200, req.body.reduce((a, b) => a + b, 0));
 	return next();
 });
 
-server.listen(process.env.RESTIFY_SERVER_PORT);
+server.listen(8080);
