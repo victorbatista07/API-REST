cc152daa62df30efa0856af4943d6a94dee76999
victorbatista07 <joao.victor.zaga@gmail.com>
2018-10-14 20:46:17 +0000

    Remove comments and function unused

--- apiTest/index.js
+++ apiTest/index.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var app = express();
-//user = {name, x}
-//app.get('/teste/name', function(req, res, name){
-//    res.send(name);
-//})
-
-app.param(['nome', 'x'], function (req, res, next, key, value) {
-  res.json({ user: 'tobi' });
-  req.is('application/json');
- console.log(value);
-  next();
-});
 
 app.get('/teste/:name', function (req, res, next) {
-  res.json({ "user" :  req.params.name });
-  console.log("name");
-  console.log("name");
+  res.json({ "user" :  req.params.name, "x": req.params.x });
   next();
 });
 
