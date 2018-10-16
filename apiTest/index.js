31c99445825bec6e6c2aafecce8e573691b11229
joaobatista <joaobatista@catho.com.br>
2018-10-16 13:21:43 -0300

    show logs on stdout

--- apiTest/index.js
+++ apiTest/index.js
@@ -2,15 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/teste/:name', function (req, res, next) {
+  app.set('title', 'router name');
   res.json({ "user" :  req.params.name, "x": req.query.x});
   next();
 });
 
-app.listen(3008);
-
-
-
-app.get('/map', function(req, res, end){
+app.get('/map', function(req, res, next){
+  app.set('title', 'router map');
   const createMapper = require("map-factory");
   const mapper = createMapper();
 
@@ -23,17 +21,20 @@ app.get('/map', function(req, res, end){
     "z" : [456]
   };
 
-  console.log(src);
-
   mapper.map("nome").to("name")
   mapper.map("x.y").to("y")
   mapper.map("z").to("z.teste")
   mapper.map("x.k").to("z.eita")
 
   var result = mapper.execute(src);
-
   res.send(result);
-
-  console.log(result);
-  end();
+  next();
 });
+
+app.use(function (req, res, next) {
+  var now = new Date();
+  console.log("Data:", now.toLocaleDateString(), "Horas:", now.toLocaleTimeString(), "status code:", res.statusCode, "rota:", app.get('title'));
+  next();
+});
+
+app.listen(3008);
