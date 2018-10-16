c50ef59345346ef9183e19915fbb8ac5268591ca
victorbatista07 <joao.victor.zaga@gmail.com>
2018-10-16 01:56:20 +0000

    working with map factorial

--- apiTest/index.js
+++ apiTest/index.js
@@ -7,3 +7,33 @@ app.get('/teste/:name', function (req, res, next) {
 });
 
 app.listen(3008);
+
+
+
+app.get('/map', function(req, res, end){
+  const createMapper = require("map-factory");
+  const mapper = createMapper();
+
+  var src = {
+    "nome" : "Teste",
+    "x" : {
+      "y" : 123,
+      "k" : "oi",
+    },
+    "z" : [456]
+  };
+
+  console.log(src);
+
+  mapper.map("nome").to("name")
+  mapper.map("x.y").to("y")
+  mapper.map("z").to("z.teste")
+  mapper.map("x.k").to("z.eita")
+
+  var result = mapper.execute(src);
+
+  res.send(result);
+
+  console.log(result);
+  end();
+});
